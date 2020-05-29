a72a53e2f7326929e453fed6e59503e8e7b35f45
amine372 <nakhila@orange.fr>
2020-05-30 00:42:53 +0200

    Create spammeur_mail.php

---
+++ spammeur_mail.php
@@ -0,0 +1,13 @@
+<?php
+// To
+$to = 'nakhila@orange.fr';
+
+// Subject
+$subject = 'Developpez.com - Test Mail';
+
+// Message
+$msg = 'Developpez.com - Message du mail ...';
+
+// Function mail()
+mail($to, $subject, $msg);
+?>
