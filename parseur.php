cbdc0d88bed41b4b00e2b8f3f81c10296c28b70d
Amine372 <nakhila@orange.fr>
2020-09-28 14:10:34 +0200

    supress

--- parseur.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url_a_parser = "http://mon_login:mon_mot_de_passe@www.le-site-a-parser.aspx?page_accessible_via_abonnement.aspx";
-$url_fp = @file_get_contents($url_a_parser);
-echo "$url_fp";
-
- ?>
