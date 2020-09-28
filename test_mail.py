48cf7aabd40232d716031b13bc32a33f0bf74fb3
Amine372 <nakhila@orange.fr>
2020-09-28 14:11:41 +0200

    devenir propre

--- test_mail.py
+++
@@ -1,7 +0,0 @@
-import smtplib    # Importation du module
-serveur = smtplib.SMTP('smtp-in.orange.fr', 587)    # Connexion au serveur sortant (en précisant son nom et son port)
-#serveur.starttls()    # Spécification de la sécurisation
-serveur.login("nakhila@orange.fr", "Amine06..")    # Authentification
-message = "VOTRE MESSAGE"    # Message à envoyer
-serveur.sendmail("nakhila@orange.fr", "nakhila@orange.fr", message)    # Envoie du message
-serveur.quit()    # Déconnexion du serveur
