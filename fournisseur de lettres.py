431748a7affcda3f8aa051850f5b8dd5337e6508
amine372 <nakhila@orange.fr>
2020-09-29 17:53:05 +0200

    Create fournisseur de lettres.py

---
+++ fournisseur de lettres.py
@@ -0,0 +1,8 @@
+import string string.letters 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+import random
+a =random.choice(string.letters)
+print(a)
+#mon_fichier = open("fichier.txt", "w")
+#mon_fichier.write("Premier test d'écriture dans un fichier via Python")
+#50
+#mon_fichier.close()
