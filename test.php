1ce5a6f38d38c7a371c89a6d4432c0c4db75a3bb
amine372 <nakhila@orange.fr>
2020-09-29 17:28:27 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
-$page = 'www.facebook.fr';
- $sec = "1";
- header("Refresh: $sec; url=$page");
-
- ?>
+$page = "www.facebook.fr";
+$sec = "1";
+?>
+<html>
+    <head>
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+    </head>
+    <body>
+    <?php
+        echo "Watch the page reload itself in 10 second!";
+    ?>
+    </body>
+</html>
