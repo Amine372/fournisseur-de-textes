4858996db555f5d3a7923351d2d89acacffeac87
amine372 <nakhila@orange.fr>
2020-09-29 20:20:52 +0200

    Create fournisseurs_de_poemes.py

---
+++ fournisseurs_de_poemes.py
@@ -0,0 +1,19 @@
+import random
+import string
+ver1 =["le roi de la pampa retourne sa chemise","Lorsque Tout est fini, lorsuque l'on agonise","le cheval parthénon s'énerve sur sa frise","le vieux marin breton pris sa prise","c'était à cinq o'clock que sortait la marquise","du jeune avantageux, la nymphe était éprise","il se penche, il voulait attraper sa valise","quand l'un avec l'autre aussitot sympathise","lorsqu'un jour énorme, l'aède prosaise","le marbre pour l'acide est une friandise"]
+ver2 =["pour la mettre au sécher aux cornes des tauraux","lorsque le marbrier astique nos tombeaux","Depuis que lors Elgin négligea ses naseaux","pour de fin du fond du nez exciter les arceaux","pour consommer un thé puis des petits gateaux","snob un peu sur les bords des bords fondamentaux","que convoitait, c'est sur une horde d'escorc","se faire il se pourrait bien que ce soit des jumeaux","pour déplaire aux profanes aussi bien qu'aux idiots", "d'aucuns par dessus tout prisent les escargots"]
+ver3 =["le cornedbeef en boite empeste la remise","des etres indécis vous parlent sans franchise","Le mondialiste de ce temps pataugeait dans sa crise","Sur l'antique bahut, il choisit sa cerise","le chauffeur indigène attendait la brise","Une toge, il portait qui n'était pas de mise","il se penche et alors à sa grande surprise","la découverte, alors voilà qui traumatise","la critique lucide apercoit ce qu'il vise","sur la place, un forain de feu se gargarise"]
+ver4 =[]
+ver5 =[]
+ver6 =[]
+ver7 =[]
+ver8 =[]
+ver9 =[]
+ver10 =[]
+ver11 =[]
+ver12 =[]
+ver13 =[]
+ver14 =[]
+for i in range(0,14):
+    a = random.randint(0,13)
+    print(ver1[a])
