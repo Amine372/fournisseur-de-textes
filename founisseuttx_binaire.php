ea70950435bd07b66e17563ee6061bfd8fa837fb
amine372 <nakhila@orange.fr>
2021-02-03 17:26:08 +0100

    Update founisseuttx_binaire.php

--- founisseuttx_binaire.php
+++ founisseuttx_binaire.php
@@ -2,10 +2,11 @@
 set_time_limit(0);
 $ascii = array("0","1");
 $i =0;
-
+$ressource = fopen('texte_binaire.txt', 'w+');
+file_put_contents('exemple.txt', 'Ecriture dans un fichier');
 for ($i = 1; $i <= 10000; $i++)
 {
-  $ressource = fopen('texte_binaire.txt', 'w+');
+  file_put_contents('exemple.txt', "\n**NOUVEAU TEXTE**", FILE_APPEND)
 $chif = rand(5, 15);
 }
 
