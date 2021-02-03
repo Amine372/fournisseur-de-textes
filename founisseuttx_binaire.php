86165b894d92374bb45d943f1d632e42c361976d
amine372 <nakhila@orange.fr>
2021-02-03 17:28:28 +0100

    hbj,n;

--- founisseuttx_binaire.php
+++ founisseuttx_binaire.php
@@ -5,10 +5,10 @@ $i =0;
 $ressource = fopen('texte_binaire.txt', 'w+');
 file_put_contents('texte_binaire.txt', 'Voici le texte');
 file_put_contents('texte_binaire.txt', "\n", FILE_APPEND);
-for ($i = 1; $i <= 10000; $i++)
+for ($i = 1; $i <= 10; $i++)
 {
-  file_put_contents('texte_binaire.txt', "\n**NOUVEAU TEXTE**", FILE_APPEND);
-$chif = rand(5, 15);
+  $chif = rand(0, 1);
+  file_put_contents('texte_binaire.txt', $chif, FILE_APPEND);
 }
 
 
