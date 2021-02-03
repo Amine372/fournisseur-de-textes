81d8a68e91cd87d29ceb911f98d463fedb718a85
amine372 <nakhila@orange.fr>
2021-02-03 17:22:53 +0100

    km,:

--- founisseuttx_binaire.php
+++ founisseuttx_binaire.php
@@ -1,10 +1,11 @@
 <?php
+set_time_limit(0);
 $ascii = array("0","1");
 $i =0;
-$max =2;
 
 for ($i = 1; $i <= 10000; $i++)
 {
+  $ressource = fopen('texte_binaire.txt', 'w+');
 $chif = rand(5, 15);
 }
 
