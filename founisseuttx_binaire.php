6cf8a8109146a218a48e983bed3e9ac1cfd7889e
amine372 <nakhila@orange.fr>
2021-02-03 21:18:19 +0100

    jk;,

--- founisseuttx_binaire.php
+++ founisseuttx_binaire.php
@@ -2,9 +2,9 @@
 set_time_limit(0);
 $ascii = array("0","1");
 $i =0;
-$ressource = fopen('texte_binaire.txt', 'w+');
-file_put_contents('texte_binaire.txt', 'Voici le texte');
-file_put_contents('texte_binaire.txt', "\n", FILE_APPEND);
+//$ressource = fopen('texte_binaire.txt', 'w+');
+//file_put_contents('texte_binaire.txt', 'Voici le texte');
+//file_put_contents('texte_binaire.txt', "\n", FILE_APPEND);
 for ($i = 1; $i <= 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000; $i++)
 {
   $chif = rand(0, 1);
